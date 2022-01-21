68735579c482e9debd0cc03399bacfd269088f51
ABoni14 <aboni150@gmail.com>
2022-01-21 17:15:01 +0100

    add top header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.6.1/css/all.min.css" integrity="sha512-BxQjx52Ea/sanKjJ426PAhxQJ4BPfahiSb/ohtZ2Ipgrc5wyaTSgTwPhhZ/xC66vvg+N4qoDD1j0VcJAqBTjhQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="{{asset('css/app.css')}}">
   <title>Document</title>
 </head>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,12 @@
 <header>
+  <div class="top-header">
+    <div class="container">
+      <div class="contain-addict">
+        <h4>DC POWER VISA<i class="far fa-registered"></i></h4>
+        <h4>ADDITIONAL DC SITES<i class="fas fa-sort-down"></i></h4>
+      </div>
+    </div>
+  </div>
   <div class="container header-contain">
     <img src="{{asset("img/dc-logo.png")}}" alt="">
     <ul class="header-nav">
